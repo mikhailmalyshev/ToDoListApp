370efdad9c2f5b1db42d186ce79edac07845a7e8
Mikhail Malyshev <malyshev090994@gmail.com>
2020-04-12 18:06:48 +0300

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Михаил Малышев on 12/04/2020.
+//  Copyright © 2020 Mikhail Malyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
